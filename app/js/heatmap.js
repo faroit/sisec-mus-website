4408e20199674c69a179da183809cf83ac4489ef
Nils Werner <nils.werner@audiolabs-erlangen.de>
2016-10-31 16:33:45 +0100

    All the animations

--- app/js/heatmap.js
+++ app/js/heatmap.js
@@ -50,31 +50,30 @@ function rect(data) {
   // Draw rectangles here
   // render only as many rows as there are methods (using array length instead of data)
   heatmap.enter().append("rect")
-      .attr("width", function(d) { return w; })
-      .attr("height", function(d) { return h; })
-    .merge(heatmap)
-      // shift them for numbers of rows available
-      .attr("y", function(d, i) { return (i - 1) * h; })
-      // fill with defined colorscale
-      .style("fill", function(d) { return colorScale(d.score); })
-      // render tooltip data
-      .on("mouseover", function(d) {
+    .attr("width", function(d) { return w; })
+    .attr("height", function(d) { return h; })
+    // shift them for numbers of rows available
+    .attr("y", function(d, i) { return (i - 1) * h; })
+    // fill with defined colorscale
+    .style("fill", function(d) { return colorScale(d.score); })
+    // render tooltip data
+    .on("mouseover", function(d) {
+       tooltip.transition()
+         .duration(200)
+         .style("opacity", .9);
+       tooltip.html(
+         "Track: "  + d.track_id +
+         "<br/>Method: " + headers.methods[d.estimate_name] +
+         "<br/>" + headers.metrics[d.metric] + ': ' + d.score +
+         "<br/>" + headers.targets[d.target_name]
+       )
+       d3.selectAll(".method_label").classed("active", function(x) { return d.estimate_name == x.key; });
+      })
+     .on("mouseout", function(d) {
          tooltip.transition()
            .duration(200)
-           .style("opacity", .9);
-         tooltip.html(
-           "Track: "  + d.track_id +
-           "<br/>Method: " + headers.methods[d.estimate_name] +
-           "<br/>" + headers.metrics[d.metric] + ': ' + d.score +
-           "<br/>" + headers.targets[d.target_name]
-         )
-         d3.selectAll(".method_label").classed("active", function(x) { return d.estimate_name == x.key; });
-        })
-       .on("mouseout", function(d) {
-           tooltip.transition()
-             .duration(200)
-             .style("opacity", 0);
-        });
+           .style("opacity", 0);
+      });
 
     // delete rectangles
     heatmap.exit().remove();
@@ -85,81 +84,110 @@ function rect(data) {
 // TODO: Add sort based on mean, so that better methods appears on top
 // TODO: Make IBM render on top in any case. Add spacing for IBM
 function update(data) {
+  var t = d3.transition()
+    .duration(950);
   // read https://bost.ocks.org/mike/join/ to understand the concept of update and enter
 
   // group by track_id
   var tracks = d3.nest()
     .key(function(d) { return d.track_id; })
+    .sortKeys(d3.ascending)
     .entries(data);
 
   // group by method_name (=estimate_name)
   var methods = d3.nest()
     .key(function(d) { return d.estimate_name; })
+    .sortKeys(d3.ascending)
     .entries(data);
 
   // bind data to class
   var track_column = g.selectAll("g")
-    .data(tracks);
+    .data(tracks, function(d) { return d.key; });
 
   var method_label = svg.selectAll("text.method_label")
-    .data(methods);
+    .data(methods, function(d) { return d.key; });
 
-  // render method axis labels
+  // update method_label
+  method_label
+      .text(function(d) { return headers.methods[d.key]})
+      .transition(t)
+      .attr("y", function(d, i) { return (i + 2) * h - 3; })
+
+  // exit method_label
   method_label.enter()
       .append("text")
       .attr("class", "method_label")
-    .merge(method_label)
-      .text(function(d) { return headers.methods[d.key]})
       .attr("x", 45)
+      .style("text-anchor", "end")
+      .text(function(d) { return headers.methods[d.key]})
       .attr("y", function(d, i) { return (i + 2) * h - 3; })
-      .style("text-anchor", "end");
+      .style("fill-opacity", 1e-6)
+      .transition(t)
+      .style("fill-opacity", 1);
 
-  // remove method labels
+  // remove method_label
   method_label.exit()
+    .transition(t)
+    .style("fill-opacity", 1e-6)
     .remove();
+
+  track_column
+    .transition(t)
+    .attr("transform", function(d, i) { return "translate(" + i * w + ", 0)"; })
+    .each(rect);
 
   // render actual column data (=tracks) into group elements
   var track_column_enter = track_column.enter()
     .append("g")
-    .merge(track_column)
 
-  track_column_enter
-    .append("text")
+  track_column_enter.append("text")
     .attr("class", "track_label")
     .attr("y", -28)
-    .style("text-anchor", "start");
+    .style("text-anchor", "start")
+    .text(function(d) { return d.key });
 
-  track_column_enter.select("text")
+  track_column.select("text")
     .text(function(d) { return d.key });
 
   track_column_enter
-      .on("click", function(dclick) {
-        // // render new site that displays more detailed track information
-        // update(data.filter(function(d) {
-        //   return d.track_id == dclick.key;
-        // }));
-      })
-      .on("mouseover", function(dclick) {
-        // d3.selectAll(".track").classed("active", function(d) { return d.key == dclick.key; });
-        d3.selectAll(".grid .track_label").classed("active", function(d) { return d.key == dclick.key; });
-        d3.selectAll(".grid .method_label").classed("active", function(d) { return d.key == dclick.key; });
-        // d3.selectAll(".track:not(.active) rect").attr("width", function(d, i) { return "5"; });
-      })
-      .attr("transform", function(d, i) { return "translate(" + i * w + ", 0)"; })
-      .each(rect);
+    .on("click", function(dclick) {
+      // // render new site that displays more detailed track information
+      // update(data.filter(function(d) {
+      //   return d.track_id == dclick.key;
+      // }));
+    })
+    .on("mouseover", function(dclick) {
+      // d3.selectAll(".track").classed("active", function(d) { return d.key == dclick.key; });
+      d3.selectAll(".grid .track_label").classed("active", function(d) { return d.key == dclick.key; });
+      d3.selectAll(".grid .method_label").classed("active", function(d) { return d.key == dclick.key; });
+      // d3.selectAll(".track:not(.active) rect").attr("width", function(d, i) { return "5"; });
+    })
+    .attr("transform", function(d, i) { return "translate(" + i * w + ", -1000)"; })
+    .style("fill-opacity", 1e-6)
+    .transition(t)
+    .attr("transform", function(d, i) { return "translate(" + i * w + ", 0)"; })
+    .style("fill-opacity", 1)
+    .each(rect);
 
   track_column.exit()
+    .transition(t)
+    .style("fill-opacity", 1e-6)
+    .attr("transform", function(d, i) { return "translate(" + i * w + ", 1000)"; })
     .remove();
 }
 
 // encapsulate init data
 function init(data) {
   update(data.filter(function(d) {
-    return d.target_name == 2 && d.metric == 2 && d.track_id >= 51;
+    return d.target_name == 2 && d.metric == 2 && d.track_id >= 31;
   }));
-  setTimeout(function () {
+  setInterval(function () {
+    var randval = Math.random() * 100;
+    var randval2 = Math.random() * 100;
+    var randmin = Math.min(randval, randval2);
+    var randmax = Math.max(randval, randval2);
     update(data.filter(function(d) {
-      return d.target_name == 0 && d.metric == 2 && d.track_id < 20;
+      return d.target_name == Math.floor(Math.random() * 2) && d.metric == 2 && d.track_id >= randmin && d.track_id < randmax;
     }));
-  }, 1000);
+  }, 5000);
 }

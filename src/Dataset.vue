eab436edb1ea1241bb8b7bdc8deb15a68e066e49
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2016-12-19 21:58:38 +0100

    add text for dataset description

--- src/Dataset.vue
+++ src/Dataset.vue
@@ -1,7 +1,48 @@
 <template>
   <div class="hero-body">
-      <div class="container has-text-centered">
-          Presenting the DSD100 Dataset
+      <div class="container">
+        <div class="column is-8 is-offset-2">
+          <h1 class="title">The DSD100 Dataset</h1>
+        </div>
+        <div class="columns is-mobile">
+          <div class="column is-8 is-offset-2">
+
+The data set consists of a total of 100 full-track songs of different
+styles and includes the mixtures and four original sources: </br>
+
+<nav class="level">
+  <div class="level-item has-text-centered">
+    <p class="title is-5">Vocals</p>
+  </div>
+  <div class="level-item has-text-centered">
+    <p class="title is-5">Drums</p>
+  </div>
+  <div class="level-item has-text-centered">
+    <p class="title is-5">Bass</p>
+  </div>
+  <div class="level-item has-text-centered">
+    <p class="title is-5">Other</p>
+  </div>
+</nav>
+
+The dataset is evenly split divided between a development subset and a test subset.
+
+The <a href="#/Methods">participants</a> are then t
+separation function and return the estimated sources and the performance
+results, and send us back the performance results
+
+The dataset called Demixing Secrets Dataset 100 (DSD100).
+The sources for MSD100 were created from stems downloaded
+from The <a href="http://www.cambridge-mt.com/ms-mtk.htm">
+‘Mixing Secrets’ Free Multitrack Download Library.</a>
+We would like to thank Mike Senior, not only for giving
+us the permission to use this multitrack material, but also
+for maintaining such resources for the audio community.
+
+<p><a class="button is-primary">Download Dataset (14 Gb)</a></p>
+
+          </div>
+        </div>
       </div>
   </div>
 </template>

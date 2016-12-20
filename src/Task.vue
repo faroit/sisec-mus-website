39dabf51cfdcdf0efba47b09c2057e76cf8b8afe
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2016-12-20 20:39:12 +0100

    changes

---
+++ src/Task.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hero-body">
+    task
+  </div>
+</template>
+
+<script>
+import bulma from 'bulma/css/bulma.css';
+
+export default {
+}
+</script>
+
+<style media="screen">
+</style>

23687b40648622878dae9d4fc11b97e749acea12
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2016-12-30 18:48:49 +0100

    add boxplots

--- src/Results.vue
+++ src/Results.vue
@@ -1,46 +1,66 @@
 <template>
   <div class="hero-body">
-    <table class="table">
-  <thead>
-    <tr>
-      <th>Method Shortname</th>
-      <th>Authors</th>
-      <th>Affiliation</th>
-      <th>Supervised Method</th>
-      <th>Uses Data-Augmentation</th>
-      <th>Code</th>
-      <th>More</th>
-    </tr>
-  </thead>
-  <tbody>
-  <tr v-for="record in json">
-    <td>{{record.short}}</td>
-    <td>{{record.authors}}</td>
-    <td>{{record.affiliation}}</td>
-    <td class="is-icon">
-      <span v-if="record.is_supervised">
-        <i class="fa fa-check"></i>
-      </span>
-    </td>
-    <td class="is-icon">
-      <span v-if="record.uses_augmentation">
-        <i class="fa fa-check"></i>
-      </span>
-    </td>
-    <td class="is-icon">
-      <a v-if="record.code" :href="record.code">
-        <i class="fa fa-code"></i>
-      </a>
-    </td>
-    <td class="is-icon">
-      <a v-if="record.references" :href="record.code">
-        <i class="fa fa-eye"></i>
-      </a>
-    </td>
-  </tr>
-  </tbody>
-</table>
+    <div class="container">
+    <h1 class="title is-2">Objective Evaluation Results</h1>
+    <div class="content">
+      The participants were asked to provide estimates for the separated targets
+      for all mixtures. The quality of these estimates was then evaluated by
+      comparison with the true source images using four objective
+      performance criteria. For more information about the evaluation metrics see <a href='http://bass-db.gforge.inria.fr/bss_eval/'>BSSeval</a>.
+    </div>
+    <div class='content  has-text-centered'>
+      <div class="card">
+        <table class="table">
+            <tr>
+              <td>SDR</td>
+              <td>Signal to Distortion Ratio</td>
+            </tr>
+            <tr>
+              <td>ISR</td>
+              <td>Image to Spatial distortion Ratio </td>
+            </tr>
+            <tr>
+              <td>SIR</td>
+              <td>Source to Interference Ratio</td>
+            </tr>
+            <tr>
+              <td>SAR</td>
+              <td>Sources to Artifacts Ratio</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="content">
+      <p>In the following we present boxplots of all tracks grouped by
+        <strong>target source</strong>, <strong>Metric</strong> and
+        <strong>Subset (Test/Train)</strong>
+      </p>
+    </div>
+      <section>
+        <div class="container">
+          <h1 class='title'>Vocals</h1>
+          <img src="./assets/vocals.png" alt="Description">
+        </div>
+      </section>
+      <section>
+        <h1 class='title'>Accompaniment</h1>
+        <img src="./assets/accompaniment.png" alt="Description">
+      </section>
+      <section>
+        <h1 class='title'>Drums</h1>
+        <img src="./assets/drums.png" alt="Description">
+      </section>
+      <section>
+        <h1 class='title'>Bass</h1>
+        <img src="./assets/bass.png" alt="Description">
+      </section>
+      <section>
+        <h1 class='title'>Other</h1>
+        <img src="./assets/other.png" alt="Description">
+      </section>
   </div>
+</div>
 </template>
 
 <script>

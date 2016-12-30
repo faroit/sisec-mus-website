5531745c5cac3b782ecabc615f440a43f6732ccd
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2016-12-30 19:35:49 +0100

    add credits

--- src/About.vue
+++ src/About.vue
@@ -1,6 +1,84 @@
 <template>
   <div class="hero-body">
-    about
+    <div class="content">
+      <h1 class='title'>Authors of this website</h1>
+      <article class="media">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a href="https://www.audiolabs-erlangen.de/fau/assistant/stoeter"><strong>Fabian-Robert Stöter</strong></a>, International Audio Laboratories Erlangen
+            <br>
+            <a href="https://github.com/faroit/dsdtools">dsdtools</a>, Statistical Evaluation, Website
+            <br>
+          </p>
+        </div>
+      </div>
+    </article>
+    <article class="media">
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <a href="https://www.audiolabs-erlangen.de/fau/assistant/werner"><strong>Nils Werner</strong></a>, International Audio Laboratories Erlangen
+          <br>
+            masters the modern web (vue.js, webpack...). This site would look and feel like from 1999 without him.
+          <br>
+        </p>
+      </div>
+    </div>
+  </article>
+  <article class="media">
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <a href="https://members.loria.fr/ALiutkus/"><strong>Antoine Liutkus</strong></a>, INRIA Nancy - Grand Est
+        <br>
+          Organizer of 2016 SiSEC.
+        <br>
+      </p>
+    </div>
+  </div>
+  </article>
+    </div>
+    <div class="content">
+      <h1 class='title'>Sisec Software</h1>
+      <ul>
+        <li><a href="https://github.com/faroit/sisec-mus-website">
+          <span><i class="fa fa-github"></i></span>
+          <span>Source Code for this Website</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/faroit/sisec-mus-results">
+          <span><i class="fa fa-github"></i></span>
+          <span>Results and Statistical Evaluation</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/faroit/dsdtools">
+          <span><i class="fa fa-github"></i></span>
+          <span>Python parser for the DSD100 dataset</span>
+          </a>
+        </li>
+
+        <li><a href="https://github.com/faroit/sisec-mus-previews">
+          <span><i class="fa fa-github"></i></span>
+          <span>Select excerpts for 30s preview</span>
+          </a>
+        </li>
+        <li><a href="https://github.com/faroit/sisec-mus-trim">
+          <span><i class="fa fa-github"></i></span>
+          <span>Trim estimates based on 30s preview selection</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="content">
+      <h1 class='title'>Additional Software</h1>
+      <ul>
+        <li><a href="https://vuejs.org/">vue.js</a></li>
+        <li><a href="https://github.com/naomiaro/waveform-playlist">waveform-playlist</a></li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
